69bd4e4eebb34d2d87dfa1a7bc9d7be06b285dd6
beeftongue551 <login@beeftongue551.org>
2022-06-19 23:24:11 +0900

    【v1.0対応】Google Analytics 対応

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import {Head, Html, Main, NextScript} from "next/document";
+import {GA_TRACKING_ID} from '../lib/gtag'
 
 const Document = () => {
   return (
@@ -10,6 +11,22 @@ const Document = () => {
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
         <link rel="stylesheet"
               href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
       </Head>
       <body>
       <Main />
