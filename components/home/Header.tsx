12066204963f40181bb1a4d3f2d4bfccc494d588
beeftongue551 <login@beeftongue551.org>
2022-06-21 22:06:01 +0900

    【v1.5対応】headerボタン準備

--- components/home/Header.tsx
+++ components/home/Header.tsx
@@ -1,19 +1,30 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import {Button} from "react-bootstrap";
+
 
 const Header: React.FC = (): JSX.Element => {
 
-  const bgBlue = {
-    backgroundColor: '#5383E8'
+  const blueButtonStyle = {
+    backgroundColor: '#5383E8',
+    border: '#5383E8'
   }
 
   return (
-    <nav className="navbar navbar-expand-lg" style={bgBlue}>
+    <nav className="navbar navbar-expand-lg" style={{backgroundColor: '#5383E8'}}>
       <div className="container-fluid">
         <Link href={"/"} passHref >
-          <Image src="/TitleLog.png" alt="タイトルロゴ" height={60} width={130} objectFit="contain" />
+          <Image src="/TitleLog.png" alt="タイトルロゴ" height={60} width={150} objectFit="contain" />
         </Link>
+
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="hoverable" aria-current="page" href="/market"><Button style={blueButtonStyle}><h6>Market</h6></Button></Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   )
