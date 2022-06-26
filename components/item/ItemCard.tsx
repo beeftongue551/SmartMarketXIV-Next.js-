89818bbd74c7323a2c10fb934cd52b390ecb9800
beeftongue551 <login@beeftongue551.org>
2022-06-26 23:25:59 +0900

    【v1.5対応】Market詳細表示機能の作成: アイテム表示用カードの作成

---
+++ components/item/ItemCard.tsx
@@ -0,0 +1,34 @@
+import {NextPage} from "next";
+import {ItemDataType} from "../../types/ItemDataType";
+import {Card, Col, Row} from "react-bootstrap";
+import XivIcon from "../other/XivIcon";
+
+type Props = {
+  item: ItemDataType
+}
+
+const ItemCard: NextPage<Props> = (props: Props): JSX.Element => {
+  const { item } = props
+
+  return (
+    <div>
+      <Card className="mx-auto" style={{width: "90%"}}>
+        <Row>
+          <Col xs={2} className="mx-auto"><XivIcon icon={item.itemIcon} alt={item.itemName} size={60}/></Col>
+          <Col>
+            <Row>
+              <Col xs={4} style={{color: "#758592"}}><h4>{item.itemLevel}</h4></Col>
+              <Col xs={8}><h4>{item.itemName}</h4></Col>
+              <Col>{item.itemCategory}</Col>
+              {
+                item.equipmentLevel !== 1 &&
+                <Col style={{color: "#4171d6"}}>装備レベル: <b>{item.equipmentLevel}</b></Col>
+              }
+            </Row>
+          </Col>
+        </Row>
+      </Card>
+    </div>
+  )
+}
+export default ItemCard
--- pages/market/detail.tsx
+++ pages/market/detail.tsx
@@ -1,8 +1,11 @@
 import {NextPage} from "next";
 import {useRouter} from "next/router";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import SelectServer from "../../components/other/SelectServer";
 import styles from '../../styles/Home.module.css'
+import ItemCard from "../../components/item/ItemCard";
+import {ItemDataType} from "../../types/ItemDataType";
+import {getItemByItemId} from "../../api/beef/ItemApi";
 
 const Detail: NextPage = ():JSX.Element => {
 
@@ -11,16 +14,33 @@ const Detail: NextPage = ():JSX.Element => {
   const { id, dataCenter } = router.query
   let selectedDataCenter = 'Mana'
   if(dataCenter !== undefined) {
-    if (typeof dataCenter === "string") {
+    if (typeof dataCenter === 'string') {
       selectedDataCenter = dataCenter
     }
   }
+
+
+  const [itemData, setItemData] = useState<ItemDataType>({
+    equipmentLevel: 0, id: 0, itemCategory: "", itemCategoryIcon: "", itemIcon: "", itemLevel: "", itemName: "", jobCategoryName: "", recipeId: 0
+  })
+
+  useEffect(() => {
+    //THINK: 無限に呼び出される
+    if (typeof id === "string") {
+      getItemByItemId(Number(id)).then((itemData) => {
+        setItemData(itemData)
+      })
+    }
+  },[id])
+
   const [server, setServer] = useState("Mana")
 
   return (
     <div className={styles.body}>
-
+      <ItemCard item={itemData} />
+      <br/>
       <SelectServer dataCenter={selectedDataCenter} setServer={setServer} />
+
     </div>
   )
 }
