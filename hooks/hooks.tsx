b6660b5aa0d4544d5eb55be3b2081086237ec10e
beeftongue551 <login@beeftongue551.org>
2022-06-27 23:22:17 +0900

    【v1.5対応】Market詳細表示機能の作成: Market情報取得用customhooksの作成

--- hooks/hooks.tsx
+++ hooks/hooks.tsx
@@ -2,6 +2,8 @@ import {useEffect, useState} from "react";
 import {NewsDataType} from "../types/NewsDataType";
 import {getNewsListLimit} from "../api/beef/NewsApi";
 import NewsList from "../components/home/NewsList";
+import {MarketDataType} from "../types/ItemDataType";
+import {getMarketByIDs} from "../api/universalis/MarketApi";
 
 export const useNews = (): JSX.Element => {
   const [news, setNews] =useState<NewsDataType[]>([])
@@ -18,4 +20,50 @@ export const useNews = (): JSX.Element => {
   },[])
 
   return <NewsList newsData={news}></NewsList>
+}
+
+export const useMarket: (worldOrDc: string, ids: (number | number[])) => MarketDataType = (worldOrDc: string, ids: number | number[]) => {
+  const [market, setMarket] = useState<MarketDataType>({
+      averagePrice: 0,
+      averagePriceHQ: 0,
+      averagePriceNQ: 0,
+      currentAveragePrice: 0,
+      currentAveragePriceHQ: 0,
+      currentAveragePriceNQ: 0,
+      dcName: "",
+      hqSaleVelocity: 0,
+      itemID: 0,
+      lastUploadTime: 0,
+      listings: [],
+      maxPrice: 0,
+      maxPriceHQ: 0,
+      maxPriceNQ: 0,
+      minPrice: 0,
+      minPriceHQ: 0,
+      minPriceNQ: 0,
+      nqSaleVelocity: 0,
+      recentHistory: [],
+      regularSaleVelocity: 0,
+      stackSizeHistogram: undefined,
+      stackSizeHistogramHQ: undefined,
+      stackSizeHistogramNQ: undefined,
+      worldUploadTimes: undefined
+  })
+
+  useEffect(() => {
+    const itemIds: number[] = []
+    if(typeof ids === "number") {
+      itemIds.push(ids)
+    } else {
+      ids.map((id: number) => {
+        itemIds.push(id)
+      })
+    }
+    getMarketByIDs(itemIds, worldOrDc).then((marketData) => {
+      setMarket(marketData)
+    })
+  },[ids, worldOrDc])
+
+
+  return market
 }
