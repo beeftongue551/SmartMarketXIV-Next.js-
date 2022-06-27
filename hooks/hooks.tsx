5d54344764119309bec241090843644caba10117
beeftongue551 <login@beeftongue551.org>
2022-06-27 22:09:51 +0900

    【v1.5対応】Market詳細表示機能の作成: Market情報取得カスタムフックの作成

--- hooks/hooks.tsx
+++ hooks/hooks.tsx
@@ -2,6 +2,9 @@ import {useEffect, useState} from "react";
 import {NewsDataType} from "../types/NewsDataType";
 import {getNewsListLimit} from "../api/beef/NewsApi";
 import NewsList from "../components/home/NewsList";
+import {MarketDataType} from "../types/ItemDataType";
+import {getMarketByIDs} from "../api/universalis/MarketApi";
+import {type} from "os";
 
 export const useNews = (): JSX.Element => {
   const [news, setNews] =useState<NewsDataType[]>([])
@@ -18,4 +21,30 @@ export const useNews = (): JSX.Element => {
   },[])
 
   return <NewsList newsData={news}></NewsList>
+}
+
+/**
+ * アイテムIDからマーケット情報を取得する
+ *
+ * @param worldOrDc ワールド名 or データセンタ名
+ * @param ids アイテムID
+ */
+export const useMarket = (worldOrDc: string, ids: number | number[]) => {
+  const [market, setMarket] = useState<MarketDataType>()
+
+  useEffect(() => {
+    const itemIds: number[] = []
+    if(typeof ids === "number") {
+      itemIds.push(ids)
+    } else {
+      ids.map((id) => {
+        itemIds.push(id)
+      })
+    }
+    getMarketByIDs(itemIds, worldOrDc).then((marketData: MarketDataType) => {
+      setMarket(marketData)
+    })
+  }, [worldOrDc, ids])
+
+  return market
 }
