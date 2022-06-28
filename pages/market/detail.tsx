01553a706dd3259a7433d1f4d8497994d8c726da
beeftongue551 <login@beeftongue551.org>
2022-06-28 22:48:06 +0900

    【v1.5対応】Market詳細表示機能の作成: Market詳細表示画面にLayoutが適用されてなかった問題の修正

--- pages/market/detail.tsx
+++ pages/market/detail.tsx
@@ -7,6 +7,7 @@ import ItemCard from "../../components/item/ItemCard";
 import {ItemDataType} from "../../types/ItemDataType";
 import {getItemByItemId} from "../../api/beef/ItemApi";
 import PriceList from "../../components/market/PriceList";
+import Layout from "../../components/layout";
 
 const Detail: NextPage = ():JSX.Element => {
 
@@ -38,10 +39,12 @@ const Detail: NextPage = ():JSX.Element => {
 
   return (
     <div className={styles.body}>
-      <ItemCard item={itemData} />
-      <br/>
-      <SelectServer dataCenter={selectedDataCenter} setServer={setServer} />
-      <PriceList itemId={itemData.id} worldOrDc={server} />
+      <Layout title={itemData.itemName} description={itemData.itemName + "のFF14内マーケットの詳細情報ページです"}>
+        <ItemCard item={itemData} />
+        <br/>
+        <SelectServer dataCenter={selectedDataCenter} setServer={setServer} />
+        <PriceList itemId={itemData.id} worldOrDc={server} />
+      </Layout>
     </div>
   )
 }
