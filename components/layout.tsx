7fde870b8929e7320a50f355d342fae34ca69831
beeftongue551 <login@beeftongue551.org>
2022-07-02 09:57:58 +0900

    【v1.0Fixed】不要なcssファイルの削除

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,7 +2,6 @@ import {NextPage} from "next";
 import React from "react";
 import Head from "next/head";
 import Header from "./home/Header";
-import '../styles/layout.module.css';
 
 type PageInfo = {
   children?: React.ReactNode
